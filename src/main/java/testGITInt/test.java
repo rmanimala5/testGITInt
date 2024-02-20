25d979aca53623c4fde21da424f1ddb2ad4158d4
mani <rmanimala5@gmail.com>
2024-02-20 09:18:27 +0530

    Annotations has been added to the test file

--- src/main/java/testGITInt/test.java
+++ src/main/java/testGITInt/test.java
@@ -1,11 +1,16 @@
 package testGITInt;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
 public class test {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("test");
-
+	@Test
+	public void test1() {
+		
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.com");
 	}
 
 }
