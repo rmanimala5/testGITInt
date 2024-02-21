96823fdb6d767877e0fc54afde9cd5c17a17643d
rmanimala5 <43721926+rmanimala5@users.noreply.github.com>
2024-02-21 10:45:49 +0530

    Update test.java

--- src/main/java/testGITInt/test.java
+++ src/main/java/testGITInt/test.java
@@ -7,7 +7,7 @@ import org.testng.annotations.Test;
 public class test {
 
 	@Test()
-	public void test1() {
+	public void test1(priority=0) {
 		
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.google.com");
