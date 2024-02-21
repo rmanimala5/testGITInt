594b8ed63691694a8a119067d4ca03cbbfea671e
mani <rmanimala5@gmail.com>
2024-02-21 18:43:29 +0530

    "test"

--- src/main/java/testGITInt/test.java
+++ src/main/java/testGITInt/test.java
@@ -4,13 +4,18 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class test {
 
-	@Test
+	@Test(priority=0)
 	public void test1() {
 		
+		WebDriverManager.chromedriver().setup();
+		System.out.println("executed!!");
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.google.com");
+		System.out.println("completed!!");
 	}
 
 }
