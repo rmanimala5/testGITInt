4c712f95e2bce5e9c2320247dda8ef14629a921d
rmanimala5 <43721926+rmanimala5@users.noreply.github.com>
2024-12-24 08:26:17 +0530

    Update test.java from remote
    added a comment in test.java from remote repository

--- src/main/java/testGITInt/test.java
+++ src/main/java/testGITInt/test.java
@@ -8,6 +8,7 @@ import org.testng.annotations.Test;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class test {
+	//adding a comment
 	WebDriver driver;
 	@Test(priority=0)
 	public void test1() {
